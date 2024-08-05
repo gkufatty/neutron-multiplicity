2db063d92b30b176de1bb990d837a25268d0afb5
lmlepin9 <lml24d@fsu.edu>
2024-08-05 16:25:02 -0700

    Adding backtracking in utils and example notebook

--- examples/utils.py
+++ examples/utils.py
@@ -13,7 +13,6 @@ Utility functions to run
 the example notebooks
 '''
 
-
 def load_keys(keys_set):
     if(keys_set=="n-Ar"):
         input_keys = "./cfg/caf_keys.txt"
@@ -59,6 +58,56 @@ def load_dataset(n_files,location):
             df = pd.concat([df, df_temp])
             counter+=1
     return df 
+
+
+
+def reco_backtrack(df_test,ixn_index):
+    n_ixn = len(df_test['rec.common.ixn.dlp.id'])
+    n_particles = df_test['rec.common.ixn.dlp.part.dlp..length'][ixn_index]
+    if(ixn_index==0):
+        n_pre = 0
+    else: 
+        n_pre = np.sum(df_test['rec.common.ixn.dlp.part.dlp..length'][:ixn_index]) 
+    print('Number of interactions in this event ',n_ixn)
+    print('Printing info for interaction ', ixn_index)
+    print(f'This interaction has {n_particles} reco particles')
+
+    # Scan over particles associated to this ixn 
+    for ip in range(n_pre,n_pre + n_particles):
+        #print(ip)
+        # Check if particle is primary 
+        is_primary = df_test['rec.common.ixn.dlp.part.dlp.primary'][ip]
+        if(is_primary==False):
+            continue 
+        else:
+            # Get reco PDG
+            reco_pdg = df_test['rec.common.ixn.dlp.part.dlp.pdg'][ip]
+            print(f"Particle {ip} is a {reco_pdg} primary")
+            if(reco_pdg==2212):
+                truth_length = df_test['rec.common.ixn.dlp.part.dlp.truth..length'][ip]
+                truth_pre = np.sum(df_test['rec.common.ixn.dlp.part.dlp.truth..length'][:ip])
+                print(f'With {truth_length} overlapping particles')
+                max_overlap = 0
+                best_match_idx = 0
+                best_match_type = 0
+                # Need to do the same trick as for scan over reco particles
+                for tp in range(truth_pre,truth_pre+truth_length):
+                    temp_overlap = df_test['rec.common.ixn.dlp.part.dlp.truthOverlap'][tp]
+                    temp_tp_match = df_test['rec.common.ixn.dlp.part.dlp.truth.part'][tp]
+                    temp_tp_type = df_test['rec.common.ixn.dlp.part.dlp.truth.type'][tp] # 1 prim and 3 second
+                    print(f'True ({temp_tp_type}) particle {temp_tp_match} has this amount of overlap {temp_overlap}')
+                    if(temp_overlap>max_overlap):
+                        max_overlap=temp_overlap
+                        best_match_idx=temp_tp_match
+                        best_match_type=temp_tp_type
+
+
+                if(best_match_type==3):
+                    best_match_pdg = df_test['rec.mc.nu.sec.pdg'][best_match_idx]
+                    print('true secondary, PDG of best match: ', best_match_pdg)
+                elif(best_match_type==1):
+                    best_match_pdg = df_test['rec.mc.nu.prim.pdg'][best_match_idx]
+                    print('true primary, PDG of best match: ', best_match_pdg)
 
 
 class ParticleCode():
