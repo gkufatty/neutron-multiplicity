f6465780a1330bae1ca4249fc699143a29bc3dc4
gkufatty <133120205+gkufatty@users.noreply.github.com>
2025-07-10 11:56:09 -0500

    Add files via upload

--- analysis/nu_selection/sources/main.cc
+++ analysis/nu_selection/sources/main.cc
@@ -1,1 +1,218 @@
+// ======================
+// HEADER SECTION
+// ======================
+
+// ROOT dependencies
+#include "TTree.h"
+#include "TSystem.h"  // For gSystem
+#include "TFile.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TChain.h"
+#include "TEfficiency.h"
+#include "TCanvas.h"
+#include "TVector3.h"
+
+#include "TGraph.h"
+#include "TLegend.h"
+#include <TAxis.h>
+#include "TGaxis.h"
+#include "TSystem.h"  // For gSystem
+#include <array>
+#include <string>
+#include <map> 
+#include <stdexcept>
+#include <cmath>  // For TMath functions
+#include <utility> // For std::pair
+#include <set>
+
+// Standard dependencies
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unistd.h>
+#include <vector>
+
+// CAF dependencies
+#include "duneanaobj/StandardRecord/StandardRecord.h" //Ideally, this should be SRProxy.h, but there is an include error for that now. Alternatively, you can use SetBranchStatus function in TreeLoader, but it does not work for the common branch (to do)
+
+#include "DUNEStyle.h"
+#include "cuts.h"  // Include the header file for cuts
+#include "structures.h"  // Include the header file for structures
+#include "utils.h"  // Include the header file for utils
+
+
+// ======================
+// FUNCTION DECLARATIONS
+// ======================
+
+
+
+// Main workflow
+int select_2x2_nu(std::string input_file_list, const char* version ="6.2", const char* mode = "RHC");
+
+// ======================
+// MAIN FUNCTION
+// ======================
+
+
+int main(int argc, char** argv){
+
+    char cwd[1024];
+    if(getcwd(cwd, sizeof(cwd)) != NULL) {
+        std::cout << "Current working directory: " << cwd << std::endl;
+    }
+
+    if(argc != 3){
+        std::cout << "\nUSAGE: " << argv[0] << " <input_caf_file_list> <mode>\n" << std::endl;
+        return 1;
+    }
+
+    // std::string input_file_list = argv[1];
+    
+    std::string version = argv[1];
+    std::string mode = argv[2];
+    std::string input_file_list = "input_files/MiniRun" + version + "_"+ mode+".txt";
+    std::cout << "Using MiniRun" << version << " in " << mode << " mode." << std::endl;
+    select_2x2_nu(input_file_list, version.c_str(), mode.c_str());
+
+    
+    return 0;
+}
+
+// ======================
+// FUNCTION DEFINITIONS
+// ======================
+
+// === True Analysis Functions ===
+
+
+
+
+int select_2x2_nu(std::string input_file_list,const char* version, const char* mode){
+    std::cout << "Running neutrino selector..." << std::endl;
+
+    std::ifstream caf_list(input_file_list.c_str());
+    if (!caf_list.is_open()) {
+        std::cerr << Form("File %s not found", input_file_list.c_str()) << std::endl;
+        return 1;
+    }
+
+    // Count CAF files
+    int total_files = 0;
+    std::string line;
+    while (std::getline(caf_list, line)) total_files++;
+    std::cout << "Total number of CAF trees = " << total_files << std::endl;
+
+    caf_list.clear();
+    caf_list.seekg(0, std::ios::beg);
+
+    // Load into TChain
+    TChain *caf_tree = new TChain("cafTree");
+    std::string tmp;
+    while (caf_list >> tmp) {
+        caf_tree->Add(tmp.c_str());
+    }
+
+    // Link CAF
+    caf::StandardRecord* sr = new caf::StandardRecord;
+    caf_tree->SetBranchAddress("rec", &sr);
+    int tree_entries = caf_tree->GetEntries();
+    std::cout << "Number of entries: " << tree_entries << std::endl;
+
+
+    caf::StandardRecord* out_sr = new caf::StandardRecord();
+
+    // Stats
+    Counters stats;
+    std::set<std::string> unique_file_paths;
+    std::vector<std::tuple<std::string, int, int, int,bool>> passed_events;
+    std::vector<TrueVertexSelection_np> np_true_vertices;
+    // Loop over entries
+    for (int n = 0; n < tree_entries; n++) {
+        caf_tree->GetEntry(n);
+        std::string full_path = caf_tree->GetFile()->GetName();  // Use full path
+
+        auto& all_vertices = np_true_vertices;
+        for (size_t i = 0; i < sr->mc.nu.size(); i++) {
+            if (QELikeSignal(sr, i, mode)) {
+                stats.nu_true++;
+                stats.prim_neutrons += sr->mc.nu[i].nneutron;
+                int event_id = sr->meta.nd_lar.event;
+                auto np_list = look_sec_particles(i, sr,stats);
+                all_vertices.insert(all_vertices.end(), np_list.begin(), np_list.end());
+            }
+
+        }
+
+        bool save_event = false;
+        for (size_t i = 0; i < sr->common.ixn.dlp.size(); ++i) {
+            auto [signal, best_match_idx] = process_reco_interaction(sr, i);
+            if (best_match_idx == -1) continue;
+            if (signal){
+                stats.nu_reco++;
+                save_event = true;
+                bool matched_true_signal = QELikeSignal(sr, best_match_idx, mode);
+                if (matched_true_signal) {
+                    stats.matched++;
+                }
+                passed_events.emplace_back(full_path, sr->meta.nd_lar.event, i, best_match_idx, matched_true_signal);  // Use full path here
+            }
+        }
+
+        if (save_event){
+            unique_file_paths.insert(full_path);
+        }
+    }
+
+    // Print stats
+    std::cout << "Total true QE-like interactions: " << stats.nu_true << std::endl;
+    std::cout << "Total reconstructed QE-like interactions: " << stats.nu_reco << std::endl;
+    std::cout << "Total matched QE-like interactions: " << stats.matched << std::endl;  
+    std::cout << "Num of true neutron induced protons: " << stats.np_true << std::endl;
+    std::cout << "Num of true secondary protons: " << stats.secp_true << std::endl;
+
+    // Save stats to CSV
+    std::string stats_filename = "output_files/MiniRun" + std::string(version) + "_" + std::string(mode) + "_stats.csv";
+    std::ofstream stats_out(stats_filename);
+    if (!stats_out.is_open()) {
+        std::cerr << "Error opening stats CSV file: " << stats_filename << std::endl;
+        return 1;
+    }
+    float nu_efficiency = static_cast<double>(stats.matched) / stats.nu_true;
+    float nu_purity = static_cast<double>(stats.matched) / stats.nu_reco;
+    std::cout << "Nu efficiency: " << nu_efficiency << std::endl;
+    std::cout << "Nu purity: " << nu_purity << std::endl;
+    stats_out << "nu_true,nu_reco,eff, pur, prim_neutrons,np_true,secp_true\n";
+    stats_out << stats.nu_true << ","
+              << stats.nu_reco << ","
+              << nu_efficiency << ","
+              << nu_purity << ","
+              << stats.prim_neutrons << ","
+              << stats.np_true << ","
+              << stats.secp_true << "\n";
+    stats_out.close();
+
+    // Write filtered event list
+    std::string out_filename_ixn = "output_files/MiniRun" + std::string(version) +"_"+std::string(mode) + "_QE_like_ixn.txt";
+    std::ofstream evt_out(out_filename_ixn);
+    evt_out << "file_name,event,vtx_r,vtx_t,matched_true_signal\n";
+    for (const auto& [f, evt, vtx_r, vtx_t, matched_true_signal] : passed_events)
+        evt_out << f << "," << evt << "," << vtx_r << "," << vtx_t << "," << matched_true_signal << "\n";
+    evt_out.close();
+
+    std::string out_filename = "../n_selection/output_files/MiniRun" + std::string(version) +"_"+std::string(mode) + "QE_like.txt";
+    std::ofstream caf_out(out_filename);
+    for (const auto& path : unique_file_paths)
+        caf_out << path << "\n";
+    caf_out.close();
+
+    std::cout << "Saved " << passed_events.size() << " QE-like true interactions in " << unique_file_paths.size() << " CAF files" << std::endl;
+    std::cout << "There are " << stats.nu_true << " true QE-like interactions." << std::endl;
+    std::cout << "There are " << stats.nu_reco << " reconstructed QE-like interactions with " << stats.matched << " matching true signal definition."<< std::endl;
+    return 0;
+}
+
+
+
 
---
+++ analysis/nu_selection/sources/utils.cc
@@ -0,0 +1,193 @@
+#include "cuts.h"
+#include "structures.h" 
+#include "utils.h"  // Include the header file for utils
+#include "duneanaobj/StandardRecord/StandardRecord.h" //Ideally, this
+#include <cassert>
+#include <cstring>
+#include <utility> // for std::pair
+
+// ======================
+// FUNCTION IMPLEMENTATIONS
+// =======================
+
+
+
+int FindVertexBestMatch(
+    const caf::SRVector3D& vertex,
+    const std::vector<std::size_t>& vtx_overlaps_idx,
+    const std::vector<float>& vtx_overlaps,
+    const caf::StandardRecord* sr){
+    int best_match_idx = -1;
+    float max_overlap = -1;
+    
+    for(size_t i = 0; i < vtx_overlaps.size(); i++) {
+        float current_overlap = vtx_overlaps[i];
+        size_t current_idx = vtx_overlaps_idx[i];
+        
+        if(current_overlap > max_overlap) {
+            max_overlap = current_overlap;
+            best_match_idx = current_idx;
+        }
+        
+        if(best_match_idx != -1) {
+            auto best_match_vertex = sr->mc.nu[best_match_idx].vtx;
+            float delta_x = abs(best_match_vertex.x - vertex.x);
+            float delta_y = abs(best_match_vertex.y - vertex.y);
+            float delta_z = abs(best_match_vertex.z - vertex.z);
+            
+            if(!(delta_x < 5 && delta_y < 5 && delta_z < 5)) {
+                best_match_idx = -1;
+            }
+        }
+    }
+    return best_match_idx;
+}
+
+
+PartBestMatch FindParticleBestMatch(
+    const int reco_pdg,
+    const int reco_type,
+    const std::vector<caf::TrueParticleID>& overlaping_particles, 
+    const std::vector<float>& overlaps,
+    const caf::StandardRecord* sr, 
+    bool verbose){   
+    PartBestMatch bm{};
+
+    bm.coincidence = false;
+    float max_overlap = -1.0f;
+
+    for(size_t n_part =0; n_part < overlaping_particles.size(); n_part++){
+        float current_overlap = overlaps[n_part];
+        int truth_ixn_idx = overlaping_particles[n_part].ixn;
+        int truth_part_idx = overlaping_particles[n_part].part;
+        auto truth_type = overlaping_particles[n_part].type;
+        if(current_overlap>max_overlap){
+            max_overlap = current_overlap;
+            bm.interaction_idx = truth_ixn_idx;
+            bm.particle_idx = truth_part_idx;
+            bm.type = truth_type;
+        }
+    }
+
+    if (bm.type == 1) {  // Primary particle
+        bm.pdg = sr->mc.nu[bm.interaction_idx].prim[bm.particle_idx].pdg;
+        bm.time = sr->mc.nu[bm.interaction_idx].prim[bm.particle_idx].time;
+        bm.start = sr->mc.nu[bm.interaction_idx].prim[bm.particle_idx].start_pos;
+        bm.end = sr->mc.nu[bm.interaction_idx].prim[bm.particle_idx].end_pos;
+        bm.parent = sr->mc.nu[bm.interaction_idx].prim[bm.particle_idx].parent;
+        bm.energy= sr->mc.nu[bm.interaction_idx].prim[bm.particle_idx].p.E;
+        bm.length = (bm.end-bm.start).Mag();
+        bm.distance = (bm.start-sr->mc.nu[bm.interaction_idx].vtx).Mag();
+    } 
+    else if (bm.type == 3) {  // Secondary particle
+        bm.pdg = sr->mc.nu[bm.interaction_idx].sec[bm.particle_idx].pdg;
+        bm.time = sr->mc.nu[bm.interaction_idx].sec[bm.particle_idx].time;
+        bm.start = sr->mc.nu[bm.interaction_idx].sec[bm.particle_idx].start_pos;
+        bm.end = sr->mc.nu[bm.interaction_idx].sec[bm.particle_idx].end_pos;
+        bm.parent = sr->mc.nu[bm.interaction_idx].sec[bm.particle_idx].parent;
+        bm.energy= sr->mc.nu[bm.interaction_idx].sec[bm.particle_idx].p.E;
+        bm.length = (bm.end-bm.start).Mag();
+        bm.distance = (bm.start-sr->mc.nu[bm.interaction_idx].vtx).Mag();
+    }
+    //std::cout << bm.coincidence << std::endl;
+    if((reco_pdg==bm.pdg) && (reco_type==bm.type)){
+        bm.coincidence = true;
+        //std::cout << bm.coincidence << std::endl;
+    }
+    return bm;
+}
+
+void process_truth_interactions(
+    const caf::StandardRecord* sr, 
+    Counters& stats, 
+    int tree_entry, 
+    const char* file_name, 
+    const char* mode){
+    for (size_t i = 0; i < sr->mc.nu.size(); i++) {
+        if (QELikeSignal(sr, i, mode)) {
+            std::cout << "Found a QELike signal in event " << sr->meta.nd_lar.event << std::endl;
+            stats.nu_true++;
+            int mode = sr->mc.nu[i].mode;
+            switch(mode){
+                case 1:stats.QES++; break;
+                case 10:stats.MEC++; break;
+                case 4:stats.RES++; break;
+            }
+        }
+    }
+}
+
+
+// === Reco Analysis Functions ===
+
+std::pair<bool,int> process_reco_interaction(
+    caf::StandardRecord* sr, int i){
+    auto vertex = sr->common.ixn.dlp[i].vtx;
+    auto vtx_overlaps = sr->common.ixn.dlp[i].truthOverlap;
+    auto vtx_overlaps_idx = sr->common.ixn.dlp[i].truth; //index of the true interaction
+    const auto& reco = sr->common.ixn.dlp[i];
+    // Vertex matching
+    int best_match_idx = FindVertexBestMatch(vertex, vtx_overlaps_idx, vtx_overlaps, sr);
+    if (best_match_idx == -1) return {false, -1};
+    return {RecoIsQELikeSignal(sr, i), best_match_idx};
+}
+
+
+std::vector<TrueVertexSelection_np> look_sec_particles(
+    const int vtx_idx, 
+    caf::StandardRecord* sr,
+    Counters& stats) {
+    const auto& nu = sr->mc.nu[vtx_idx];
+    const auto& vtx_pos = nu.vtx;
+    std::vector<TrueVertexSelection_np> result{}; 
+    //iterate over the secondary particles of the vertex
+    for (size_t j = 0; j < nu.sec.size(); ++j) {
+        std::vector<int> n_induced_pdgs;
+
+        bool n_induced = false;
+        int pdg =nu.sec[j].pdg;
+        const caf::SRVector3D start = nu.sec[j].start_pos;
+        const caf::SRVector3D end = nu.sec[j].end_pos;
+        //skip particles not fully contained in the FV
+        if(!( kIsVtxFV(start)) || !(kIsVtxFV(end))) continue;
+
+        if(was_neutron_induced(vtx_idx, j, sr)){
+            n_induced_pdgs.push_back(pdg);
+            n_induced = true;
+        }
+
+
+        if(pdg == ParticleCode::proton){
+            stats.secp_true++;
+            if(n_induced){
+                stats.np_true++;
+                TrueVertexSelection_np vtx_np{};
+                vtx_np.vtx = vtx_idx;
+                vtx_np.insignal = true;
+                vtx_np.neutron_t0 = nu.sec[j].time;
+                vtx_np.proton_t0 = nu.sec[j].time;
+                vtx_np.proton_energy = nu.sec[j].p.E*1000 - ParticleCode::proton_mass;
+                vtx_np.proton_length = (nu.sec[j].start_pos - nu.sec[j].end_pos).Mag();
+                vtx_np.proton_distance = (nu.sec[j].start_pos - vtx_pos).Mag();
+                result.push_back(vtx_np);
+            }
+        }
+    }
+    return result;
+} 
+
+bool was_neutron_induced(int vtx_idx, int part_idx, const caf::StandardRecord* sr){
+    bool neutron_induced = false;
+    const auto& nu = sr->mc.nu[vtx_idx];
+    const int parent_id = nu.sec[part_idx].parent;
+    for(int i =0; i<nu.prim.size(); i++){
+        //std::cout << "prim pdg" << nu.prim[i].pdg  << std::endl;
+        if (nu.prim[i].G4ID == parent_id){
+            //std::cout << "parent pdg" << nu.prim[i].pdg  << std::endl;
+            if(nu.prim[i].pdg == 2112){
+                neutron_induced = true;
+            }
+        }
+    }
+    return neutron_induced;
+}
