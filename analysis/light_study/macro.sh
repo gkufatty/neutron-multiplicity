90fb02043b947941573d5967bb706e9b4f77cec8
gkufatty <133120205+gkufatty@users.noreply.github.com>
2025-07-10 12:11:18 -0500

    Add files via upload

---
+++ analysis/light_study/macro.sh
@@ -0,0 +1,19 @@
+mkdir -p output_files
+
+# Compile from the 'sources' directory
+g++ -std=c++17 -O2 -g -pedantic -Wall \
+    sources/main.cc sources/utils.cc sources/cuts.cc \
+    -o select_n \
+    -Iheaders \
+    `root-config --cflags --glibs` \
+    -I/cvmfs/dune.opensciencegrid.org/products/dune/duneanaobj/v03_06_01b/include \
+    -I$SRPROXY_INC \
+    -L$DUNEANAOBJ_LIB -lduneanaobj_StandardRecordProxy \
+    -lduneanaobj_StandardRecord \
+    -lduneanaobj_StandardRecord_dict
+
+if [ $? -eq 0 ]; then
+    echo "Compilation successful"
+else
+    echo "Compilation failed"
+fi
