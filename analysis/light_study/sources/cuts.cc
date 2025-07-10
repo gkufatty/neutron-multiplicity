36e318c786db30271a5b21fb82f1881c36a66e98
gkufatty <133120205+gkufatty@users.noreply.github.com>
2025-07-10 12:12:09 -0500

    Add files via upload

---
+++ analysis/light_study/sources/cuts.cc
@@ -0,0 +1,230 @@
+
+#include "cuts.h"  // Include the header file for cuts
+#include "structures.h" 
+#include <cassert>
+#include <cstring>
+#include <utility> // for std::pair
+
+bool kIsCC(const caf::StandardRecord* sr, int idx){
+    // if (sr->mc.nnu == 0) return false;
+    // assert(sr->mc.nnu == 1);
+    return sr->mc.nu[idx].iscc;
+}
+
+bool kIsNumu(const caf::StandardRecord* sr, int idx){
+    return (abs(sr->mc.nu[idx].pdg) == 14);
+}
+
+bool kIsAntiNumu(const caf::StandardRecord* sr,int idx){
+    return ( abs(sr->mc.nu[idx].pdg) == 14);
+}
+
+bool kIsVtxCont(const caf::StandardRecord* sr, int idx){
+    return (sr->mc.nu[idx].isvtxcont);
+}
+
+bool kIsVtxFV(const caf::SRVector3D& vertex){
+    //const auto& vertex = sr->mc.nu[0].vtx;  // Fix the missing vertex definition
+    return (
+        std::abs(vertex.x) < constants::X_BOUND - constants::FV_DIST &&
+        std::abs(vertex.x) > constants::TPC_DIST &&
+        std::abs(vertex.y) < constants::Y_BOUND - constants::FV_DIST &&
+        std::abs(vertex.z) < constants::Z_BOUND - constants::FV_DIST &&
+        std::abs(vertex.z) > constants::FV_DIST
+    );
+}
+
+bool kIsArgon(const caf::StandardRecord* sr, int idx){
+    return ((sr->mc.nu[idx].targetPDG) == ParticleCode::argon);
+} 
+
+bool AngleCrossMx2(const caf::StandardRecord* sr, int idx) {
+    const auto& nu = sr->mc.nu[idx];
+    const auto& p = nu.prim[0];
+    if(abs(p.pdg)!= ParticleCode::muon) return false;
+    double muon_angle;
+    double dz = p.start_pos.z - p.end_pos.z;
+    double dx = p.start_pos.x - p.end_pos.x;
+    double dy = p.start_pos.y - p.end_pos.y;
+    double track_length = std::sqrt(dx * dx + dy * dy + dz * dz);
+    if (track_length > 0) muon_angle = std::abs(dz / track_length);
+    else muon_angle = 1.0;
+    return muon_angle > 0.9;
+}
+
+bool EnergyCrossMx2(const caf::StandardRecord* sr, int idx) {
+    const auto& nu = sr->mc.nu[idx];
+    const auto p = nu.prim[0];
+    if (abs(p.pdg) != ParticleCode::muon) return false;  
+   // muon_energy = p.p.E;
+    return p.p.E > 1.0 ;
+}
+
+
+bool IsBasicCCNuAr(const caf::StandardRecord* sr, int idx, const char* mode) {
+    const auto& nu = sr->mc.nu[idx];
+    if (strcmp(mode, "RHC") == 0){
+        return ( kIsCC(sr,idx) && kIsAntiNumu(sr,idx)  && kIsVtxFV(nu.vtx) && kIsArgon(sr,idx) && EnergyCrossMx2(sr,idx)&& AngleCrossMx2(sr,idx) );
+    }
+    if(strcmp(mode, "FHC") == 0){
+        return ( kIsCC(sr,idx) && kIsNumu(sr,idx)  &kIsVtxFV(nu.vtx) && kIsArgon(sr,idx) && EnergyCrossMx2(sr,idx)&& AngleCrossMx2(sr,idx) );
+    }
+    return false;
+}
+
+bool ContLargeProtons(const caf::StandardRecord* sr, int idx) {
+    const auto& nu = sr->mc.nu[idx];
+    for (const auto& p : nu.prim) {
+        if (p.pdg == ParticleCode::proton) {
+            double dx = p.end_pos.x - p.start_pos.x;
+            double dy = p.end_pos.y - p.start_pos.y;
+            double dz = p.end_pos.z - p.start_pos.z;
+            double track_length = std::sqrt(dx*dx + dy*dy + dz*dz);
+            if (track_length > 2.0) return true;
+        }
+    }
+    return false;
+}
+
+bool QELikeSignal(const caf::StandardRecord* sr, int idx, const char* mode) {
+    const auto& nu = sr->mc.nu[idx];
+    return (IsBasicCCNuAr(sr,idx, mode) && !ContLargeProtons(sr, idx) && nu.nneutron > 0 && nu.npip ==0 && nu.npim == 0 && nu.npi0 == 0);   
+}
+
+
+
+bool track_minerva_match(int nixn, int npart, const caf::StandardRecord* sr){
+    bool mcOnly = true;
+    double mnvOffsetX=-10; double mnvOffsetY=5;
+    if (mcOnly){ mnvOffsetX=0; mnvOffsetY=0;}
+    int maxPartMinerva=-999; int maxTypeMinerva=-999;  
+    int maxIxnMinerva=-999;  
+    double dirZExiting=-999;
+
+
+    // Calculate 2x2 reco track quantities
+    auto start_pos=sr->common.ixn.dlp[nixn].part.dlp[npart].start;
+    auto end_pos=sr->common.ixn.dlp[nixn].part.dlp[npart].end;
+
+    double diffVertexdZ=abs(start_pos.z-sr->common.ixn.dlp[nixn].vtx.z); 
+    double diffVertexdX=abs(start_pos.x-sr->common.ixn.dlp[nixn].vtx.x);
+    double diffVertexdY=abs(start_pos.y-sr->common.ixn.dlp[nixn].vtx.y);
+    double diffVertex=TMath::Sqrt(diffVertexdZ*diffVertexdZ+diffVertexdY*diffVertexdY+diffVertexdX*diffVertexdX);
+    
+    // If track away from vertex return false
+    if(diffVertex>5) return false;
+    
+    double dX=(end_pos.x-start_pos.x);
+    double dY=(end_pos.y-start_pos.y);
+    double dZ=(end_pos.z-start_pos.z);
+    double length=TMath::Sqrt(dX*dX+dY*dY+dZ*dZ);
+    double dirX=dX/length; double dirY=dY/length; double dirZ=dZ/length;
+    // Revert direction if not properly reconstructed
+    if (dirZ<0){ dirZ=-dirZ; dirX=-dirX; dirY=-dirY; auto temp=start_pos; end_pos=start_pos; end_pos=temp;}
+
+    int minervaPass=0; double dotProductDS=-999;
+    double deltaExtrapY=-999; double deltaExtrapX=-999; 
+
+    // Loop over MINERvA ixn
+    for(int i=0; i<sr->nd.minerva.ixn.size(); i++){
+
+        // Loop over MINERvA tracks
+        for (int j=0; j<sr->nd.minerva.ixn[i].ntracks; j++){
+            double dir_z=sr->nd.minerva.ixn[i].tracks[j].dir.z;
+            double end_z=sr->nd.minerva.ixn[i].tracks[j].end.z;
+            double end_x=sr->nd.minerva.ixn[i].tracks[j].end.x;
+            double start_x=sr->nd.minerva.ixn[i].tracks[j].start.x;
+
+            double end_y=sr->nd.minerva.ixn[i].tracks[j].end.y;
+            double start_y=sr->nd.minerva.ixn[i].tracks[j].start.y;
+            double start_z=sr->nd.minerva.ixn[i].tracks[j].start.z;
+
+            int truthPart=sr->nd.minerva.ixn[i].tracks[j].truth[0].part;
+            double dXMnv=(sr->nd.minerva.ixn[i].tracks[j].end.x-sr->nd.minerva.ixn[i].tracks[j].start.x);
+            double dYMnv=(sr->nd.minerva.ixn[i].tracks[j].end.y-sr->nd.minerva.ixn[i].tracks[j].start.y);
+            double dZMnv=(sr->nd.minerva.ixn[i].tracks[j].end.z-sr->nd.minerva.ixn[i].tracks[j].start.z);
+            double lengthMinerva=TMath::Sqrt(dXMnv*dXMnv+dYMnv*dYMnv+dZMnv*dZMnv);
+                
+            if (lengthMinerva<10) return false;
+
+            double dirXMinerva=dXMnv/lengthMinerva;
+            double dirYMinerva=dYMnv/lengthMinerva;
+            double dirZMinerva=dZMnv/lengthMinerva;
+            double dotProduct=dirXMinerva*dirX+dirYMinerva*dirY+dirZ*dirZMinerva;
+            double extrapdZ=start_z-end_pos.z;
+            double extrapY=dirY/dirZ*(extrapdZ)+end_pos.y-start_y;
+            double extrapX=dirX/dirZ*(extrapdZ)+end_pos.x-start_x;
+            double diffExtrap=TMath::Sqrt(TMath::Power(extrapY-start_y,2));
+
+
+            if (dotProductDS<dotProduct && abs(extrapY-mnvOffsetY)<15  && 
+                abs(TMath::ATan(dirXMinerva/dirZMinerva)-TMath::ATan(dirX/dirZ))<0.06 && 
+                abs(TMath::ATan(dirYMinerva/dirZMinerva)-TMath::ATan(dirY/dirZ))<0.06 && 
+                abs(extrapX-mnvOffsetX)<15){ dotProductDS=dotProduct;
+                                            deltaExtrapY=extrapY;
+                                            deltaExtrapX=extrapX;
+                                            dirZExiting=dirZ;
+
+                if (mcOnly){
+                maxPartMinerva=sr->nd.minerva.ixn[i].tracks[j].truth[0].part;
+                maxTypeMinerva=sr->nd.minerva.ixn[i].tracks[j].truth[0].type;
+                maxIxnMinerva=sr->nd.minerva.ixn[i].tracks[j].truth[0].ixn;}	
+
+
+                if (end_z>300){ minervaPass=1;} if(dirZExiting<dirZ){ dirZExiting=dirZ;}
+
+                return true;
+            } 
+        
+            }   
+        }
+    if(dotProductDS==-999){
+        return false;
+    }
+
+}
+
+
+
+// Count number of primary and secondary reconstructed particles
+std::pair<int, int> getNumRecoSecPrim(const caf::StandardRecord* sr, int idx) {
+    int n_prim = 0;
+    int n_sec = 0;
+    const auto& reco = sr->common.ixn.dlp[idx];
+    for (size_t j = 0; j < reco.part.dlp.size(); ++j) {
+        const auto& p = reco.part.dlp[j];
+        (p.primary ? n_prim++ : n_sec++);
+    }
+    return {n_prim, n_sec}; 
+}
+
+std::pair<bool, bool> has_valid_muon(const caf::StandardRecord* sr, int idx) {
+    const auto& reco = sr->common.ixn.dlp[idx];
+    bool has_exiting_muon = false;
+    bool cross_mx2 = false;
+
+    for (size_t j = 0; j < reco.part.dlp.size(); ++j) {
+        const auto& p = reco.part.dlp[j];
+        if (p.pdg == ParticleCode::muon && p.primary && p.end.z > constants::Z_BOUND) {
+            has_exiting_muon = true;
+            cross_mx2 = track_minerva_match(idx, j, sr); 
+        }
+    }
+
+    return {has_exiting_muon, cross_mx2}; 
+}
+
+// Basic CC ν-Ar selection based on vertex cont
+bool RecoIsBasicCCNuAr(const caf::StandardRecord* sr, int idx) {
+    auto [has_exiting_muon, cross_mx2] = has_valid_muon(sr, idx);
+    return(has_exiting_muon && cross_mx2 && kIsVtxFV(sr->common.ixn.dlp[idx].vtx));
+}
+
+// Final reco QE-like selector with topology limits
+bool RecoIsQELikeSignal(const caf::StandardRecord* sr, int idx, int prim_limit, int sec_limit) {
+    auto [n_prim, n_sec] = getNumRecoSecPrim(sr, idx);  // 
+    return (RecoIsBasicCCNuAr(sr, idx) && (n_prim < prim_limit) && (n_sec < sec_limit) && (n_sec > 0));
+}
+
+
+
--- analysis/light_study/sources/main.cc
+++ analysis/light_study/sources/main.cc
@@ -1,1 +1,199 @@
+// ======================
+// HEADER SECTION
+// ======================
+
+// ROOT dependencies
+#include "TTree.h"
+#include "TSystem.h"  // For gSystem
+#include "TFile.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TChain.h"
+#include "TEfficiency.h"
+#include "TCanvas.h"
+#include "TVector3.h"
+
+#include "TGraph.h"
+#include "TLegend.h"
+#include <TAxis.h>
+#include "TGaxis.h"
+#include "TSystem.h"  // For gSystem
+#include <array>
+#include <string>
+#include <map> 
+#include <stdexcept>
+#include <cmath>  // For TMath functions
+#include <utility> // For std::pair
+#include <set>
+
+// Standard dependencies
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unistd.h>
+#include <sstream> 
+#include <vector>
+
+// CAF dependencies
+#include "duneanaobj/StandardRecord/StandardRecord.h" //Ideally, this should be SRProxy.h, but there is an include error for that now. Alternatively, you can use SetBranchStatus function in TreeLoader, but it does not work for the common branch (to do)
+#include "cuts.h"
+#include "structures.h"  // Include the header file for structures
+#include "utils.h"  // Include the header file for utils
+
+
+// ======================
+// FUNCTION DECLARATIONS
+// ======================
+
+// Main workflow
+int select_2x2_neutrons(std::string input_file_list, const char* version ="6.2", const char* mode = "RHC");
+
+// ======================
+// MAIN FUNCTION
+// ======================
+
+
+int main(int argc, char** argv){
+
+    char cwd[1024];
+    if(getcwd(cwd, sizeof(cwd)) != NULL) {
+        std::cout << "Current working directory: " << cwd << std::endl;
+    }
+
+    if(argc != 3){
+        std::cout << "\nUSAGE: " << argv[0] << " <input_caf_file_list> <mode>\n" << std::endl;
+        return 1;
+    }
+
+    // std::string input_file_list = argv[1];
+    
+    std::string version = argv[1];
+    std::string mode = argv[2];
+    std::string input_file_list = "../nu_selection/output_files/MiniRun" + version + "_" + mode + "_QE_like_ixn.txt";
+    std::cout << "Using MiniRun" << version << " in " << mode << " mode." << std::endl;
+    select_2x2_neutrons(input_file_list, version.c_str(), mode.c_str());
+    
+    return 0;
+}
+
+// ======================
+// FUNCTION DEFINITIONS
+// ======================
+
+
+int select_2x2_neutrons(std::string input_file_list, const char* version, const char* mode) {
+    std::cout << "Running neutrons selector..." << std::endl;
+    Counters stats;
+    std::vector<RecoProtonInfo> all_protons;
+    std::ifstream caf_list(input_file_list);
+
+    if (!caf_list.is_open()) {
+        std::cerr << "Error: File not found -> " << input_file_list << std::endl;
+        return 1;
+    }
+
+    // Skip header
+    std::string header;
+    std::getline(caf_list, header);
+
+    std::vector<InputCAFRow> input_rows;
+    std::string line;
+    int total_lines = 0;
+
+    while (std::getline(caf_list, line)) {
+        total_lines++;
+        std::stringstream ss(line);
+        std::string file_name, event_str, vtx_r_str, vtx_t_str, matched_str;
+
+        std::getline(ss, file_name, ',');
+        std::getline(ss, event_str, ',');
+        std::getline(ss, vtx_r_str, ',');
+        std::getline(ss, vtx_t_str, ',');
+        std::getline(ss, matched_str, ',');
+
+        if (file_name.empty() || vtx_r_str.empty()) continue;
+
+        try {
+            InputCAFRow row;
+            row.file_name = file_name;
+            row.event = std::stoi(event_str);
+            row.vtx_r = std::stoi(vtx_r_str);
+            row.vtx_t = std::stoi(vtx_t_str);
+            row.matched_true_signal = (matched_str == "1" || matched_str == "true");
+            input_rows.push_back(row);
+        } catch (...) {
+            std::cerr << "Error parsing line: " << line << std::endl;
+        }
+    }
+
+    std::cout << "Total number of CAF rows (excluding header): " << total_lines << std::endl;
+    std::cout << "Loaded " << input_rows.size() << " rows.\n";
+
+    std::set<std::string> unique_files;
+    for (const auto& row : input_rows)
+        unique_files.insert(row.file_name);
+
+    TChain* caf_tree = new TChain("cafTree");
+    for (const auto& f : unique_files)
+        caf_tree->Add(f.c_str());
+
+    std::cout << "Loaded " << unique_files.size() << " unique CAF files into TChain.\n";
+
+    auto sr = new caf::StandardRecord;
+    caf_tree->SetBranchAddress("rec", &sr);
+    int tree_entries = caf_tree->GetEntries();
+    std::cout << "Number of entries in TChain: " << tree_entries << std::endl;
+
+    for (int n = 0; n < tree_entries; n++) {
+        caf_tree->GetEntry(n);
+        std::string full_path = caf_tree->GetFile()->GetName();
+
+        for (const auto& row : input_rows) {
+            if (
+                row.file_name == full_path && row.vtx_r >= 0 &&
+                row.vtx_r < static_cast<int>(sr->common.ixn.dlp.size())
+                ){
+                const auto& target_ixn = sr->common.ixn.dlp[row.vtx_r];
+                
+                // === Step 1: find r_vtx tpc ===
+                TPC rvtx_tpc = determineTPC(target_ixn.vtx);
+                // find tpc of the vertex. 
+                // === Step 2: Iterate over all reco vertices in the event ===
+                for (size_t v = 0; v < sr->common.ixn.dlp.size(); ++v) {
+                    const auto& vtx = sr->common.ixn.dlp[v];
+
+                    // === Step 3: Loop through its particles ===
+                    for (const auto& part : vtx.part.dlp) {
+                        if (!(kIsVtxFV(part.start))|| !(kIsVtxFV(p.end))) continue;
+                        if (part.pdg == 2212 && part.primary) { // proton
+                            PartBestMatch bm = FindParticleBestMatch(part.truth, part.truthOverlap, sr, mode);
+                            if (bm.neutron_induced){
+                                stats.recovered_n_induced++;
+                                TPC part_tpc = determineTPC(part.start);
+                                //std::cout << "proton found in tpc " << tpcToString(part_tpc) << " vertex target in " << tpcToString(rvtx_tpc)<< std::endl;
+                                if(part_tpc != rvtx_tpc) {
+                                    stats.diff_tpc++;
+                                }
+                                else{
+                                    stats.same_tpc++;
+                                }
+                            }
+
+                            // Optionally: store into a RecoProtonInfo struct, etc.
+                            // RecoProtonInfo pinfo{sr->hdr.run, sr->hdr.subrun, sr->hdr.evt, v, start};
+                            // all_protons.push_back(pinfo);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    std::cout<< "Total n induced protons to recover: " << stats.recovered_n_induced << std::endl;
+    std::cout<< "Total reco protons in different TPC than vertex: " << stats.diff_tpc << std::endl; 
+    std::cout<< "Total reco protons in same TPC as vertex: " << stats.same_tpc << std::endl;
+
+    delete sr;
+    delete caf_tree;
+    return 0;
+}
 
